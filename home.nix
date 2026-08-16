a0edd849fcc188adc90a220c4468f3d6ca49bcf3
Jacob's Thinkpad T14 <jaco.pala@icloud.com>
2026-08-16 01:16:57 -0500

    bye tony

--- home.nix
+++ home.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }:
+#{ config, pkgs, ... }:
+{ pkgs, ... }:
 let
-  dotfiles = "${config.home.homeDirectory}/NixOS/cfgs";
-  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  #dotfiles = "${config.home.homeDirectory}/NixOS/cfgs";
+  #create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
   # Link config files in ./cfgs
   configs = {
     alacritty = "alacritty";
@@ -9,6 +10,7 @@ let
     nvim = "nvim";
     quickshell = "quickshell";
     sway = "sway";
+    swaylock = "swaylock";
     wofi = "wofi";
     yazi = "yazi";
   };
@@ -30,19 +32,29 @@ in
     ## CLI
     htop          # Resource monitor
     eza           # Better ls
+    fd            # Better find
+		ripgrep		    # Better grep
     gh            # GitHub CLI
     nix-search    # Package repo search
     pfetch        # System info
     sutils        # Battery & Clock commands
     weather       # Forecast
 
-    ## NVIM Editor (tony's setup)
-		neovim
-		ripgrep		    # for telescope
-		nil		        # LSP for nix lang
-		nixpkgs-fmt	  # "
-		nodejs		    # for treesitter parsers
+		#neovim
 	];
+
+  programs.neovim = {
+    enable = true;
+    extraPackages = with pkgs; [
+      nil		        # LSP for nix lang
+      nixpkgs-fmt	  # "
+      nodejs		    # for treesitter parsers
+    ];
+    plugins = with pkgs.vimPlugins; [
+      plenary-nvim
+      obsidian-nvim
+    ];
+  };
 
   # Additional init
 	home.username = "jacob";
@@ -52,7 +64,7 @@ in
 	programs.bash = {
 		enable = true;
     initExtra = ''
-      export PS1="[ \\[\\e[38;5;4m\\]\w\\[\\e[0m\\] ]\\n\\[\\e[0m\\]\\$ "
+      export PS1="\[\e[38;5;4m\]✦ \w\[\e[3m\]\n✨\[\e[0m\]"
       alias yz="yazi"
       alias ls="eza --icons"
       export PATH="$HOME/NixOS/scripts/:$PATH"     
@@ -62,7 +74,8 @@ in
   # Function to assign config files
   xdg.configFile = builtins.mapAttrs 
     (name: subpath: {
-      source = create_symlink "${dotfiles}/${subpath}";
+      #source = create_symlink "${dotfiles}/${subpath}";
+      source = ./cfgs + "/${subpath}";
       recursive = true;
     })
     configs;
