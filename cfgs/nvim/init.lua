bf9c103fc0d68c33f4c679ad49c29bd0af503d5a
Jacob's Thinkpad T14 <jaco.pala@icloud.com>
2026-08-18 16:07:13 -0500

    nvim complete

--- cfgs/nvim/init.lua
+++ cfgs/nvim/init.lua
@@ -1,1 +1,3 @@
 require("config.lazy")
+vim.cmd.colorscheme("catppuccin")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- cfgs/nvim/lua/plugins/treesitter.lua
+++ cfgs/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,35 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
-	lazy = false,
-	build = ':TSUpdate'
+	build = ':TSUpdate',
+
+	config = function()
+		local treesitter = require("nvim-treesitter")
+		treesitter.setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"html",
+				"javascript",
+				"lua",
+				"markdown",
+				"python", 
+				"query",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml"
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }
