b21280d9af21883c61fdbfb27361890992697e83
Jacob's Thinkpad T14 <jaco.pala@icloud.com>
2026-08-26 18:49:06 -0500

    fixed export cmds

--- home.nix
+++ home.nix
@@ -81,10 +81,10 @@ in
       home.stateVersion = "25.05";
       programs.bash = {
          enable = true;
-         bashrcExtra = "
-            export PS1='\[\e[38;5;4m\]✦ \w\[\e[3m\]\n✨\[\e[0m\]'
-            export PATH='$HOME/NixOS/scripts/:$PATH'  
-         ";
+         bashrcExtra = ''
+            export PS1="\[\e[38;5;4m\]✦ \w\[\e[3m\]\n✨\[\e[0m\]"
+            export PATH="$HOME/NixOS/scripts/:$PATH"  
+         '';
          shellAliases = {
             c = "clear";
             ls = "eza --icons";
